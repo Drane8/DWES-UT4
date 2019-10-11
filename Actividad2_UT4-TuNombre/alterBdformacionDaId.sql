936cca17b74af303499676222ec73daeb674de98
Dani <danigb92@hotmail.com>
2019-10-11 11:15:13 +0200

    alter2.0

--- Actividad2_UT4-TuNombre/alterBdformacionDaId.sql
+++ Actividad2_UT4-TuNombre/alterBdformacionDaId.sql
@@ -3,3 +3,10 @@ ALTER TABLE cursillos ADD PRIMARY KEY (CODCUR);
 
 -- Primary key compuesta alumnoscursillos
 ALTER TABLE alumnoscursillos ADD PRIMARY KEY (CODAL, CODCUR)
+
+--
+ALTER TABLE profesores ADD PRIMARY KEY (CODP);
+
+-- DA ERROR 
+ALTER TABLE cursillos ADD FOREIGN KEY (CODP) REFERENCES
+profesores(CODP)
